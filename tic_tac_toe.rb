9c51addd4c1f15ee1c5e0e4878ec7210d98e131b
bklynate <nathanielcfa@gmail.com>
2015-01-25 15:42:05 -0500

    A functional Tic Tac Toe game - still some bugs - its a work in progress.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -11,44 +11,56 @@ def prompt(words)
   puts ">>> #{words}"
 end
 
-def human_choice(s)
+def human_choice(board)
   prompt "Pick a position (1-9): "
   choice = gets.chomp.to_i
-  s[choice] = "X"
+  board[choice] = "X"
 end
 
-def is_space_empty(s)
-  s.select{|k,v| v == ' '}.keys
+def is_space_empty(board)
+  board.select{|k,v| v == ' '}.keys
 end
 
-def computer_choice(s)
-  choice = is_space_empty(s).sample
-  s[choice] = 'O'
+def computer_choice(board)
+  choice = is_space_empty(board).sample
+  board[choice] = 'O'
 end
 
 def draw_board(s)
   system 'clear'
   prompt "\n"
-  prompt " #{s[1]}  | #{s[2]}  | #{s[3]}"
+  prompt " #{s[1]}  | #{s[2]} | #{s[3]}"
   prompt "--------------"
-  prompt " #{s[4]}  | #{s[5]}  | #{s[6]}"
+  prompt " #{s[4]}  | #{s[5]} | #{s[6]}"
   prompt "--------------"
-  prompt " #{s[7]}  | #{s[8]}  | #{s[9]}"
+  prompt " #{s[7]}  | #{s[8]} | #{s[9]}"
   prompt "\n"
 end
 
+def check_for_winner(board)
+  winning_lines = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
+  winning_lines.each do |line|
+    return "You Won!!" if board.values_at(*line).count('X') == 3
+    return "Computer Won!!" if board.values_at(*line).count('O') == 3
+  end
+  nil
+end
+
 board = create_space
-
-
 draw_board(board)
 
-human_choice(board)
+begin
+  human_choice(board)
+  computer_choice(board)
+  draw_board(board)
+  winner = check_for_winner(board)
+end until winner || is_space_empty(board).empty?
 
-prompt draw_board(board)
-
-computer_choice(board)
-
-prompt draw_board(board)
+if winner
+  prompt winner
+else
+  prompt "It's a tie!"
+end
 
 
 
